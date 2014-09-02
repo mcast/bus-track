c59bf89de4667e183fe14f375966e4928a2b8968
Matthew Astley <beeep$mca@t8o.org>
2014-09-02 08:56:27 +0100

    gps-logger startup improvements
    put Tracks alongside tangogps ones

--- misc/gps-logger.sh
+++ misc/gps-logger.sh
@@ -7,45 +7,78 @@
 #
 # /etc/bluetooth/rfcomm.conf
 #   rfcomm0 { bind yes; device 00:0C:A5:00:E0:45; comment "ye olde navman"; }
+#
+# /etc/sudoers - append, dn't insert!
+#   mca1001		beeep = NOPASSWD: /usr/bin/rfcomm connect /dev/rfcomm0 , /usr/bin/rfcomm release /dev/rfcomm0 , /etc/init.d/gpsd stop , /etc/init.d/gpsd start , /usr/bin/killall rfcomm
+
+
+bt_is_plugged() {
+    hcitool dev | grep -qE 'hci[0-9]'
+    # Devices:
+    # 	hci0	00:0F:3D:4C:12:94
+}
+
+bt_is_conn() {
+    rfcomm show /dev/rfcomm0 > /dev/null
+    # rfcomm0: 00:0F:3D:4C:12:94 -> 00:0C:A5:00:E0:45 channel 1 connected [reuse-dlc release-on-hup tty-attached]
+}
 
 uppit() {
-    if rfcomm show /dev/rfcomm0; then
-        echo OK
+    if bt_is_conn; then
+        echo Connected OK
+        true
+    elif ! bt_is_plugged; then
+        echo Unplugged, give up connecting
         true
     else
-        sleep 8
-        sudo rfcomm connect /dev/rfcomm0 &
-        sleep 2
+        echo $( date +%Ft%T ) Trying connect...
+        sudo /usr/bin/rfcomm connect /dev/rfcomm0 &
+        sleep 3
+        bt_is_conn || sleep 7
         false
     fi
 }
 
-if rfcomm show /dev/rfcomm0; then
-    # rfcomm0: 00:0F:3D:4C:12:94 -> 00:0C:A5:00:E0:45 channel 1 connected [reuse-dlc release-on-hup tty-attached]
-    echo Already connected
-else
-    echo Nobble bluetooth
-    set -x
-    sudo /etc/init.d/gpsd stop; echo RC=$?
-    sleep 1
-    sudo rfcomm release /dev/rfcomm0; echo RC=$?
-    sleep 5
-    echo Frob bluetooth
-    sudo killall rfcomm
-    sudo rfcomm connect /dev/rfcomm0 &
-    uppit || uppit || uppit || uppit
-    set +x
-fi
+main() {
+    if bt_is_plugged; then
+        if bt_is_conn; then
+            # Already connected
+            gps_get_tracking
+            sleep 10
+        else
+            echo Nobble bluetooth, restart
+            pidof gpsd >/dev/null && sudo /etc/init.d/gpsd stop
+            sleep 1
+            sudo /usr/bin/rfcomm release /dev/rfcomm0
+            sleep 5
+            echo Frob bluetooth
+            sudo /usr/bin/killall rfcomm
+            # 6*10sec
+            uppit || uppit || uppit || uppit || uppit || uppit
+        fi
+    else
+        # Finished, for now
+        pidof gpxlogger >/dev/null && killall -INT gpxlogger
+        pidof gpsd      >/dev/null && sudo /etc/init.d/gpsd stop
+        killall -INT gpsmon # else it spins
+        sleep 5
+    fi
+}
 
-if ! pidof gpsd >/dev/null; then
-    echo Start gpsd
-    sudo /etc/init.d/gpsd start; echo RC=$?
-fi
+gps_get_tracking() {
+    if ! pidof gpsd >/dev/null; then
+        echo Start gpsd
+        sudo /etc/init.d/gpsd start
+    fi
 
+    if ! pidof gpxlogger >/dev/null; then
+        mkdir -p ~/Tracks
+        gpxlogger > ~/Tracks/track-$( date +%Ft%T ).gpx &
+        echo Started gpxlogger, pid $!
+    fi
+}
 
-if pidof gpxlogger >/dev/null; then
-    echo Logger running - leave
-else
-    gpxlogger > ~/track-$( date +%Ft%T ).gpx &
-    echo gpxlogger on $!
-fi
+while true; do
+    echo $(date +%Ft%T ) loop
+    main
+done
